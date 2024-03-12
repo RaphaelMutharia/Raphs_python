4231010a2840b02b29b7140e97cdeaa0742c90eb
munene219 <munenevictor889@gmail.com>
2024-03-12 05:22:46 -0400

    New Python

---
+++ py.py
@@ -0,0 +1,23 @@
+#python comments-this a single line comment
+"""
+this is a simple multiline comment in python
+"""
+#variables
+student_name="Jonathan"#data type is string
+student_age=20 #data type is integer
+student_fee=100.0#data type float
+student_marks=100#data type is integer
+is_male=True#data type is boolean
+#outputting the values in the variables
+print(student_name)
+print(student_age)
+print(student_fee)
+print(student_marks)
+print(is_male)
+
+STUDENT_NAME="Jonathan"
+student_name="Raph"
+print(STUDENT_NAME)
+
+x=y=z=10 #assigning one value to multiple variables
+x,y,z=30,40,50#multiple values being assigned to multiple variables
