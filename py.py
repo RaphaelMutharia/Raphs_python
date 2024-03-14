129afba18ebaccaeced64341a0f30d8de130beca
munene219 <munenevictor889@gmail.com>
2024-03-14 06:46:44 -0400

    odd numbers

--- py.py
+++ py.py
@@ -3,6 +3,7 @@
 this is a simple multiline comment in python
 """
 #variables
+
 student_name="Jonathan"#data type is string
 student_age=20 #data type is integer
 student_fee=100.0#data type float
@@ -44,3 +45,51 @@ if constituency=="Embakasi"or"Kasarani"or"Westlands":
     print("you are Governor")
 else:
     print("you are not Governor")
+#trial for even number or odd
+"""
+x=101
+y=2
+ans=x%y
+print("the answer")
+if ans==0:
+    print("Even number")
+else:
+    print("Odd number")
+"""
+#use of elif
+"""
+country=input("Enter the country:")
+if country=="Rwanda":
+    print("East Africa")
+elif country=="Kenya":
+    print("East Africa")
+elif country=="Tanzania":
+    print("East Africa")
+elif country=="Uganda":
+    print("East Africa")
+elif country=="Congo":
+    print("East Africa")
+else:
+    print("Unknown Country ")
+"""
+
+
+x=1
+while x<=10:
+    if x == 3 or x == 5:
+        x+=1
+        continue
+    print("the number is :",x)
+    x+=1
+print("loop ended")
+
+
+numbers=[1,3,5,7,9]
+total=0
+for num in numbers:
+        total+=num
+print("The sum of odd numbers from 1 to 10 is :",total)
+
+
+
+
